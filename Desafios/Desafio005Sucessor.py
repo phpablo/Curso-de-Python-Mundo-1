5acd41cbe73cd6677904292dc767d2e3ef2ea48e
phpablo <pablohenrique120@gmail.com>
2023-12-03 13:10:27 -0300

    :memo: Add files aula07a + desafio 05

---
+++ Desafios/Desafio005Sucessor.py
@@ -0,0 +1,14 @@
+#Created by : PHPablo
+#03DEZ23
+print('===========================')
+print('SUCESSOR E ANTECESSOR')
+print('==========================')
+var1 = int(input('Digite um número inteiro: '))
+ant = var1 - 1
+suc = var1 + 1
+print('===========================')
+#Linha 9 foi feita na correção do exercício
+print('O sucessor de {} é {}'.format(var1,suc))
+print('-----------------------------------')
+print('O antecessor de {} é {}'.format(var1,ant))
+print('-----------------------------------')
--- aula07a.py
+++ aula07a.py
@@ -1,3 +1,10 @@
-print(f'hello world')
-idade = 7
-print(f'hello world')
+n1 = int(input('Informe um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {} , o outro produto é {} e a divisão é {:.3f}'.format(s,m,d))
+print('Divisão inteira {} e potência {}'.format(di,e))
+
