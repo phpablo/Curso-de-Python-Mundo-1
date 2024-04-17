61ce6f2e65269ec2eedfdcf6729224f5ce5a9838
phpablo <pablohenrique120@gmail.com>
2024-04-17 15:35:14 -0300

    :memo: inclui arquivos da aula de hoje

---
+++ Desafios/Desafio006DobroTriploPow.py
@@ -0,0 +1,15 @@
+# Created by : PHPablo
+# 03DEZ23
+print("===========================")
+print("Dobro, Tripo e Raiz Quadrada")
+print("==========================")
+num = int(input("Digite um número: "))
+print("===========================")
+print(f"O dobro de {num} é {num*2}\n O triplo de {num} é {num *
+      3}\n A raiz de testando qual que coisa aki")
+print(f"A o dobro de {num} é {num} \n O Triplo de {
+      num} é {num}\n A raiz de {num} é {num}")
+
+print(f"A o dobro de {num} é {
+      num*2} \n O Triplo de {num} é {num * 3}\n A raiz de {num} é {pow(num, 1/2):.2f}")
+print("-----------------------------------")
